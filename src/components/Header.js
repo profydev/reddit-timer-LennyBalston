65f623e4b9c250101a081da6a1205178a3633195
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-02-22 15:11:12 +0000

    Create nav with links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import Flex from './Flex';
 import StyledLink from './StyledLink';
 
 function Header() {
   return (
     <header>
       <div>logo placeholder</div>
-      <StyledLink to="#">Search</StyledLink>
-      <StyledLink to="#">How it works</StyledLink>
-      <StyledLink to="#">About</StyledLink>
+      <Flex as="nav" justifyContent="flex-end" rowSpacing="xl">
+        <StyledLink to="#">Search</StyledLink>
+        <StyledLink to="/#how-it-works">How it works</StyledLink>
+        <StyledLink to="/#about">About</StyledLink>
+      </Flex>
     </header>
   );
 }
