95e2bce86da95f1304013b3248b257d0880af052
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-02-22 16:06:20 +0000

    Add logo linking to the home page to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '../assets/logo.svg';
 import Flex from './Flex';
 import StyledLink from './StyledLink';
 
 function Header() {
   return (
-    <header>
-      <div>logo placeholder</div>
+    <Flex
+      as="header"
+      height="100px"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Link to="/"><img src={logo} alt="logo" width="150px" /></Link>
       <Flex as="nav" justifyContent="flex-end" rowSpacing="xl">
         <StyledLink to="/search?q=javascript">Search</StyledLink>
         <StyledLink to="/#how-it-works">How it works</StyledLink>
         <StyledLink to="/#about">About</StyledLink>
       </Flex>
-    </header>
+    </Flex>
   );
 }
 
