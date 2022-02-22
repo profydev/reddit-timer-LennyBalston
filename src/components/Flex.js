378929d18f96e38271ec047db3ced87d39449966
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-02-22 16:01:13 +0000

    Add height and align-items properties to the Flex component

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -2,15 +2,18 @@ import styled from 'styled-components';
 
 const Flex = styled.div`
   display: flex;
+  height: ${(props) => (props.height ? props.height : 'initial')};
   flex-direction: ${(props) => (props.direction ? props.direction : 'initial')};
+  align-items: ${(props) => (props.alignItems ? props.alignItems : 'initial')};
 
   justify-content: ${(props) => (
     props.justifyContent ? props.justifyContent : 'initial'
   )};
 
   & > *  {
-    margin-right: ${(props) => (props.rowSpacing ? props.theme.spacing[props.rowSpacing] : 'initial')}
+    margin-right: ${(props) => (props.rowSpacing ? props.theme.spacing[props.rowSpacing] : 'initial')};    
   }
+
   & > :last-child {
     margin-right: initial;
   }
