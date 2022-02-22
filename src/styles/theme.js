3abdd8fe7297bd518d8c4b677f59f7af79289b1e
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-02-22 11:01:02 +0000

    Add all grey colors to the theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,6 +2,13 @@ const theme = {
   colors: {
     black: '#000000',
     white: '#ffffff',
+    greyLightest: '#f3f3f3',
+    greyLighter: '#e6e6e6',
+    greyLight: '#9e9e9e',
+    grey: '#939d8f',
+    greyDark: '#636363',
+    greyDarker: '787878',
+    greyDarkest: '1e2537',
     main: '#93918f',
     primary: '#fdb755',
     primaryDark: '#feb756',
