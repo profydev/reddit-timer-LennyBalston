db2df84978c611f9cc844f7c8188310f2ce74390
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-02-23 13:59:19 +0000

    Remove unnecessary tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,23 +7,21 @@ import StyledLink from './StyledLink';
 
 function Header() {
   return (
-    <>
-      <header>
-        <Flex
-          className="header-wrapper"
-          height="100px"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Link to="/"><img src={logo} alt="logo" width="150px" height="auto" style={{ display: 'block' }} /></Link>
-          <Flex as="nav" justifyContent="flex-end" gap="1.625rem">
-            <StyledLink to="/search?q=javascript">Search</StyledLink>
-            <StyledLink to="/#how-it-works">How it works</StyledLink>
-            <StyledLink to="/#about">About</StyledLink>
-          </Flex>
+    <header>
+      <Flex
+        className="header-wrapper"
+        height="100px"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Link to="/"><img src={logo} alt="logo" width="150px" height="auto" style={{ display: 'block' }} /></Link>
+        <Flex as="nav" justifyContent="flex-end" gap="1.625rem">
+          <StyledLink to="/search?q=javascript">Search</StyledLink>
+          <StyledLink to="/#how-it-works">How it works</StyledLink>
+          <StyledLink to="/#about">About</StyledLink>
         </Flex>
-      </header>
-    </>
+      </Flex>
+    </header>
   );
 }
 
