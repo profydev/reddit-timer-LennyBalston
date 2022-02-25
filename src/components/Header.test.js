e8ea87c96b4b9d1813507a2338eb2068e4c9e97c
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-02-25 14:31:23 +0000

    Header tests must check links within the <header> tag only

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import App from '../App';
 
 // TODO check links open correct pages, when pages are populated
+
+function getHeader() {
+  render(<App />);
+  const header = screen.getByRole('banner');
+  return header;
+}
+
 describe('Header tests', () => {
   it('Logo points to the correct page', () => {
-    render(<App />);
-    // screen.debug();
-    const link = screen.getByRole('link', { name: 'logo' });
+    const header = getHeader();
+    const link = within(header).getByRole('link', { name: 'logo' });
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', '/');
   });
 
   it('"Search" link points to the correct page', () => {
-    render(<App />);
-    const link = screen.getByRole('link', { name: /search/i });
+    const header = getHeader();
+    const link = within(header).getByRole('link', { name: /search/i });
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', '/search/javascript');
   });
 
   it('"How it works" link points to the correct page', () => {
-    render(<App />);
-    const link = screen.getByRole('link', { name: /how it works/i });
+    const header = getHeader();
+    const link = within(header).getByRole('link', { name: /how it works/i });
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', '/#how-it-works');
   });
 
   it('"About" link points to the correct page', () => {
-    render(<App />);
-    const link = screen.getByRole('link', { name: /about/i });
+    const header = getHeader();
+    const link = within(header).getByRole('link', { name: /about/i });
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', '/#about');
   });
