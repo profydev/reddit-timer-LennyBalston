85e7c9d82ce8f9871cb6f131aca5ea0ab0dcdf5c
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-03-10 16:33:06 +0000

    Rename InternalLink to StyledLink and deletd ExternalLink

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import logo from '../assets/logo-symbol.svg';
 import FooterContainer from './FooterContainer';
-import InternalLink from './InternalLink';
-import ExternalLink from './ExternalLink';
+import StyledLink from './StyledLink';
 
 function Header() {
   return (
     <FooterContainer>
-      <ExternalLink
+      <StyledLink
+        as="a"
         href="https://profy.dev/employers"
         fontSize="0.875rem"
         target="_blank"
       >
         profy.dev
-      </ExternalLink>
-      <InternalLink to="/">
+      </StyledLink>
+      <StyledLink to="/">
         <img src={logo} alt="logo" width="36px" height="auto" style={{ display: 'block' }} />
-      </InternalLink>
-      <InternalLink to="/terms" fontSize="0.875rem">Terms & Privacy</InternalLink>
+      </StyledLink>
+      <StyledLink to="/terms" fontSize="0.875rem">Terms & Privacy</StyledLink>
     </FooterContainer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import Flex from './Flex';
 import HeaderContainer from './HeaderContainer';
-import InternalLink from './InternalLink';
+import StyledLink from './StyledLink';
 
 function Header() {
   return (
     <HeaderContainer>
       <Link to="/"><img src={logo} alt="logo" width="150px" height="auto" style={{ display: 'block' }} /></Link>
       <Flex as="nav" justifyContent="flex-end" gap="1.625rem">
-        <InternalLink to="/search/javascript">Search</InternalLink>
-        <InternalLink to="/#how-it-works">How it works</InternalLink>
-        <InternalLink to="/#about">About</InternalLink>
+        <StyledLink to="/search/javascript">Search</StyledLink>
+        <StyledLink to="/#how-it-works">How it works</StyledLink>
+        <StyledLink to="/#about">About</StyledLink>
       </Flex>
     </HeaderContainer>
   );
